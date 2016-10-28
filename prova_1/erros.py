33a2b30f79864e4bf2a19b7f2fb21f41143c7a5a
Luiz <luiz@gris.dcc.ufrj.br>
2016-10-28 20:45:02 -0200

    Questão 2 pronta. Vale melhorar se tiver tempo

--- prova_1/erros.py
+++ prova_1/erros.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
+# polinomio 1 1 1 1 -47 18
+# PONTO 0.813
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -38,18 +41,55 @@ centrada = (function(x0 + H) - function(x0 - H)) / (2 * H)
 
 dy = derivada(x0)
 
-plt.plot(Ks, avancada, 'bo', label='Aprox. avancada', color='blue')
-plt.plot(Ks, atrasada, 'bo', label='Aprox. atrasada', color='green')
-plt.plot(Ks, centrada, 'bo', label='Aprox. centrada', color='red')
+difcentrada = np.array(centrada)
+difavancada = np.array(avancada)
+difatrasada = np.array(atrasada)
+difcentrada[:] = [value - dy for value in difcentrada]
+difavancada[:] = [value - dy for value in difavancada]
+difatrasada[:] = [value - dy for value in difatrasada]
+
+fig = plt.figure(figsize=(16, 12))
+
+graf1 = fig.add_subplot(221)
+graf1.plot(Ks, avancada, 'bo', label='Aprox. avancada', color='blue')
+graf1.plot(Ks, atrasada, 'bo', label='Aprox. atrasada', color='green')
+graf1.plot(Ks, centrada, 'bo', label='Aprox. centrada', color='red')
+#graf1.ylim([-38,-40])
+graf1.legend(loc='best')
+graf1.grid(True)
+graf1.set_title('Teste doido')
+
+graf2 = fig.add_subplot(222)
+graf2.plot(Ks, avancada, 'bo', label='Aprox. avancada', color='blue')
+graf2.plot(Ks, atrasada, 'bo', label='Aprox. atrasada', color='green')
+graf2.plot(Ks, centrada, 'bo', label='Aprox. centrada', color='red')
+graf2.set_ylim([-45,-30])
+graf2.legend(loc='best')
+graf2.grid(True)
+graf2.set_title('Teste doido')
+
+graf3 = fig.add_subplot(223)
+graf3.plot(Ks, avancada, 'bo', label='Aprox. avancada', color='blue')
+graf3.plot(Ks, atrasada, 'bo', label='Aprox. atrasada', color='green')
+graf3.plot(Ks, centrada, 'bo', label='Aprox. centrada', color='red')
+graf3.set_ylim([-40,-38])
+graf3.legend(loc='best')
+graf3.grid(True)
+graf3.set_title('Teste doido')
+
+graf4 = fig.add_subplot(224)
+graf4.plot(Ks, difavancada, 'bo', label='Dif. avancada', color='blue')
+graf4.plot(Ks, difatrasada, 'bo', label='Dif. atrasada', color='green')
+graf4.plot(Ks, difcentrada, 'bo', label='Dif. centrada', color='red')
+graf4.set_yscale('symlog')
+graf4.legend(loc='best')
+graf4.grid(True)
+graf4.set_title('Teste doido')
+
 # plt.plot(Ks, dy, 'r-', label='Derivada de f', color='grey') ## PREENCHER COM O VALOR DE dy
 # plt.yscale('linear', linthreshy=5*(10**-0.01))
 # plt.yscale('symlog', linthreshy=5*(10**-0.01))
 
-plt.title('Teste doido')
-plt.legend(loc='best')
-plt.grid(True)
 plt.show()
-
-# plt.yscale('symlog', linthreshy=0.05)
 
 # y_p1 = function(p1)
